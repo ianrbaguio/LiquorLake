3a96ecace63d51051511791d0f4e6427a8f9bde6
ibaguio1 <ianrey_2004@yahoo.com>
2019-02-20 12:35:08 -0700

    Liquor Lake Management System (LLMS), Product (Domain), Products(Techincal) classes added

---
+++ LiquorLake/LiquorLake/App_Code/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Products
+/// </summary>
+public class Products
+{
+    public Products()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
