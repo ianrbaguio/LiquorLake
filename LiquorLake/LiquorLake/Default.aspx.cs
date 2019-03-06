c24bed6eac6f853818923abc5a8ccb1f853e700b
Ian Rey Baguio <ronanzter@yahoo.com>
2019-03-05 20:08:58 -0700

    Added category cubes

--- LiquorLake/LiquorLake/Default.aspx.cs
+++ LiquorLake/LiquorLake/Default.aspx.cs
@@ -15,11 +15,22 @@ public partial class _Default : System.Web.UI.Page
 
         foreach(Category c in CategoryList)
         {
-            string categoryDiv = "<div style='min-width:180px; min-height:150px; display:inline-block; border: 1px solid gray; padding:25px; margin:10px;'>";
+            //string categoryDiv = "<div style='min-width:180px; min-height:150px; display:inline-block; border: 1px solid gray; padding:25px; margin:10px;'>";
+            //categoryDiv += "<a class='category_link' href='/ViewCatalog.aspx?category=" + c.CategoryID + "'>";
+            //categoryDiv += "<img src='/App_Themes/Catalog/Images/liquor_placeholder.png' style='width:100px; height:100px;'/><br/>";
+            //categoryDiv += "<b>" + c.Name + "<b>";
+            //categoryDiv += "</a>";
+            //categoryDiv += "</div>";
+
+            string categoryDiv = "<div class='square_div'>";
+            categoryDiv += "<div class='square_left'>";
             categoryDiv += "<a class='category_link' href='/ViewCatalog.aspx?category=" + c.CategoryID + "'>";
-            categoryDiv += "<img src='/App_Themes/Catalog/Images/liquor_placeholder.png' style='width:100px; height:100px;'/><br/>";
-            categoryDiv += "<b>" + c.Name + "<b>";
+            categoryDiv += "<img src='/App_Themes/Catalog/Images/liquor_placeholder.png' style='width:85px; height:75px; padding-top: 4px;'/><br/>";
+            categoryDiv += "<b>" + c.Name + "</b>";
             categoryDiv += "</a>";
+            categoryDiv += "</div>";
+            categoryDiv += "<div class='square_right'></div>";
+            categoryDiv += "<div class='square_top'></div>";
             categoryDiv += "</div>";
 
             CategoriesContainer.InnerHtml += categoryDiv;
