fc25b0ac25cce328462bdf6db33c7c3ce84dd2d4
Ajay <a.singh18@hotmail.com>
2019-03-08 12:42:16 -0700

    added new items to product table

--- LiquorLake SQL Script.sql
+++ LiquorLake SQL Script.sql
@@ -72,7 +72,21 @@ VALUES
 (088544016770,2,'Southern Comfort',27.99,750,NULL,NULL,NULL,'Sazerac Company','Single 750 ml bottle of Southern Comfort whiskey'),
 (087000150393,2,'Captain Morgan Deluxe',27.49,750,NULL,NULL,NULL,'Diageo','Single 750 ml bottle of Captain Morgan Deluxe dark rum'),
 (089540454061,2,'Kahlua',29.99,750,NULL,NULL,NULL,'Pernod Ricard' ,'Single 750 ml bottle of Kahlua coffee flavoured rum'),
-(9419227006275,3,'Kim Crawford Sauvignon',19.99,750,'New Zealand' ,'Extra Dry',NULL,'Kim Crawford','Single 750 ml bottle of Kim Crawford Sauvignon white wine')
+(9419227006275,3,'Kim Crawford Sauvignon',19.99,750,'New Zealand' ,'Extra Dry',NULL,'Kim Crawford','Single 750 ml bottle of Kim Crawford Sauvignon white wine'),
+(811538014045,2,'Virginia Black',49.99,750,NULL,NULL,NULL,'Virginia Black Llc',NULL,'Single 750ml bottle of Virginia black whiskey'),
+(087116035065,2,'Sour Puss Apple',12.49,375,NULL,NULL,NULL,'Phillips Beverage Company',NULL,'Single 375ml bottle of Sour Puss Apple liquor'),
+(087116035140,2,'Sour Puss Raspberry',12.49,375,NULL,NULL,NULL,'Phillips Beverage Company',NULL,'Single 375ml bottle of Sour Puss Raspberry liquor'),
+(087116035003,2,'Sour Puss Raspberry',25.99,1410,NULL,NULL,NULL,'Phillips Beverage Company',NULL,'Single 1.14L bottle of Sour Puss Raspberry liquor'),
+(4067700012265,2,'Jagermeister',44.99,1410,NULL,NULL,NULL,'Mast-Jagermeister Ag',NULL,'Single 1.14L bottle of Jagermeister liquor'),
+(622153625079,2,'Tanqueray Gin',59.99,1750,NULL,NULL,NULL,'Udv - Laindon',NULL,'Single 1.75L bottle of Tanqueray Gin'),
+(622153625062,2,'Tanqueray Gin',39.99,1410,NULL,NULL,NULL,'Udv - Laindon',NULL,'Single 1.14L bottle of Tanqueray Gin'),
+(622153625048,2,'Tanqueray Gin',39.99,750,NULL,NULL,NULL,'Udv - Laindon',NULL,'Single 750ml bottle of Tanqueray Gin'),
+(811538013086,2,'Kraken Spiced',18.50,375,NULL,NULL,NULL,'Proximo',NULL,'Single 375ml bottle of Kraken Black Spiced Rum'),
+(048415345439,2,'Lambs Navy',13.50,375,NULL,NULL,NULL,'Corby Spirit And Wine Limited',NUll,'Single 375ml bottle of Lambs Navy dark rum'),
+(620213025609,2,'Bacardi',13.99,375,NULL,NULL,NULL,'Bacardi',NULL,'Single 375ml bottle of Barcardi Gold rum'),
+(620213015600,2,'Bacardi',13.99,375,NULL,NULL,NULL,'Bacardi',NULL,'Single 375ml bottle of Barcardi White rum'),
+(048415345552,2,'Lambs Classic',13.50,375,NULL,NULL,NULL,'Corby Spirit And Wine Limited',NULL,'Single 375ml bottle of Lambs Classic white rum'),
+(774837595373,1,'Brava 15 Can',24.50,355,NULL,NULL,NULL,'Brava Brewing Company',NULL,'15 pack of Brava beer, 355ml per can')
 GO
 
 /*******************************************************************************************************************************
