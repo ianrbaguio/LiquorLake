747026c532a3096112d19728941a1205903a35f0
ibaguio1 <ianrey_2004@yahoo.com>
2019-04-15 14:50:27 -0600

    Updated SQL Script and Added User and Users class - Ian

--- LiquorLake/LiquorLake/Admin/Login.aspx.cs
+++ LiquorLake/LiquorLake/Admin/Login.aspx.cs
@@ -9,6 +9,13 @@ public partial class Admin_Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Users UserManager = new Users();
+        
+        int saltSize = 5;
+        string salt = UserManager.CreateSalt(saltSize);
+        string passwordHash = UserManager.CreatePasswordHash("PsiStorm1", salt);
+
+        UserManager.StoreAccountDetails("raybains", passwordHash);
 
     }
 
