9d301f1b674c4fcc1a43072c2b7ff649442a528c
Ajay <a.singh18@hotmail.com>
2019-04-15 19:57:38 -0600

    update product progress commit

---
+++ LiquorLake/LiquorLake/Admin/UpdateProduct.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LiquorLakeMasterPage.master" AutoEventWireup="true" CodeFile="UpdateProduct.aspx.cs" Inherits="Admin_UpdateProduct" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    Search by Name<asp:TextBox ID="tbSearchProduct" runat="server"></asp:TextBox>
+    <asp:Button ID="btnSearchProduct" runat="server" Text="Find Product" OnClick="btnSearchProduct_Click" />
+
+    <hr />
+
+    <asp:Table ID="ProductsTable" runat="server"></asp:Table>
+
+    <asp:Panel ID="ProductInfoPanel" runat="server" Visible="false">
+        <asp:TextBox ID="tbUPC" runat="server"></asp:TextBox>
+        <asp:TextBox ID="tbName" runat="server"></asp:TextBox>
+        <br />
+        <asp:DropDownList ID="ddlCategoryName" runat="server"></asp:DropDownList>
+        <asp:TextBox ID="tbPrice" runat="server"></asp:TextBox>
+        <asp:TextBox ID="tbSize" runat="server"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="tbCountryOfOrigin" runat="server"></asp:TextBox>
+        <asp:TextBox ID="tbWineSweetnessIndex" runat="server"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="tbCompany" runat="server"></asp:TextBox>
+        <asp:TextBox ID="tbDescription" runat="server"></asp:TextBox>
+        <br />
+        <asp:Image ID="ItemImage" runat="server" />
+        <asp:FileUpload ID="fuImage" runat="server" />
+
+        <br />
+
+        <asp:Button ID="btnUpdateProduct" runat="server" Text="Update Product" />
+    </asp:Panel>
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="jQueryPlaceHolder" Runat="Server">
+</asp:Content>
+
---
+++ LiquorLake/LiquorLake/Admin/UpdateProduct.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_UpdateProduct : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSearchProduct_Click(object sender, EventArgs e)
+    {
+        LLMS requestDirector = new LLMS();
+
+        List<Product> products = requestDirector.SearchProduct(tbSearchProduct.Text);
+
+        TableRow tr = new TableRow();
+        TableCell tc = new TableCell();
+
+        tc = new TableCell();
+        tc.Text = "";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "UPC";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "Category ID";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "Name";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "Price";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "Size(ml)";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "CountryOfOrigin";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "WineSweetnessIndex";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "Image";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "Company";
+        tr.Cells.Add(tc);
+
+        tc = new TableCell();
+        tc.Text = "Description";
+        tr.Cells.Add(tc);
+
+        ProductsTable.Rows.Add(tr);
+
+        foreach (Product p in products)
+        {
+            tr = new TableRow();
+
+            TableCell buttonCell = new TableCell();
+            Button updateButton = new Button()
+            {
+                Text = "DETAILS",
+                CssClass = "button",
+                CommandArgument = p.UPC
+            };
+
+            updateButton.Click += new EventHandler(Update_Click);
+            buttonCell.Controls.Add(updateButton);
+
+            tr.Cells.Add(buttonCell);
+
+            tc = new TableCell();
+            tc.Text = p.UPC;
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.CategoryID.ToString();
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.Name;
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.Name;
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.Size.ToString();
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.CountryOfOrigin;
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.WineSweetnessIndex;
+            tr.Cells.Add(tc);
+
+            Image i = new Image();
+            i.ImageUrl = "/Images/" + p.CategoryName + "/" + p.ImageUrl;
+            i.Width = 100;
+            i.Height = 100;
+
+            tc = new TableCell();
+            tc.Controls.Add(i);
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.Company;
+            tr.Cells.Add(tc);
+
+            tc = new TableCell();
+            tc.Text = p.Description;
+            tr.Cells.Add(tc);
+
+            ProductsTable.Rows.Add(tr);
+        }
+    }
+
+    private void Update_Click(object sender, EventArgs e)
+    {
+        Button clicked = sender as Button;
+        LLMS requestDirector = new LLMS();
+
+        ProductInfoPanel.Visible = true;
+
+        Product currentProduct = requestDirector.
+    }
+
+}
